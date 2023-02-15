edc39a27d493c198844aa3c013896a6d43863b63
Can İlgu <canilguu@gmail.com>
2023-02-15 17:22:05 +0300

    some minor changes for deployment.

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '-c1cg^-j%2l-e_-(0+ey030&yvz@^k$x@%w0pwgm#of%)qrqe+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
